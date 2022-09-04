c339a050f64b0c5180ee6c44492108d7bcef21a6
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2022-09-04 16:08:18 +0900

    Modified main.rs for issue01

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use regex::Regex;
 use std::io::{stdin, stdout, Write};
 
 fn main() {
-    let re = Regex::new(r"^https://www.amazon.co.jp/dp/[A-Z0-9]+/").unwrap();
+    let re = Regex::new(r"dp/[A-Z0-9]+/").unwrap();
+    let amazon_url = "https://www.amazon.co.jp/".to_string();
 
     let mut insert_url = String::new();
     print!("Please input amazon_url : ");
@@ -13,7 +14,7 @@ fn main() {
         .expect("Failed to read url ...");
 
     let str_text: &str = &insert_url;
-    let converted_url = re.captures(str_text).unwrap();
-
-    println!("CONVERTED_URL : {}", &converted_url[0]);
+    let caps = re.captures(str_text).unwrap();
+    let converted_url = amazon_url + &caps[0];
+    println!("CONVERTED_URL : {}", converted_url);
 }
